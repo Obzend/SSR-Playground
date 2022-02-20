81d4bfc1420b5016b206b987e3070ff78b3c4224
obzend <obz3nd@gmail.com>
2022-02-20 11:43:54 +0200

    Updated the README file. Added minor comments in webpack's base config

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SSR-Playground
+
+## Lifecycle
+
+1. Application gets rendered into a template on the server (src/index.js)
+2. The rendered application is sent to the browser
+3. The browser renderes the application, then loads the client bundle
+4. Client bundle boots up (src/client.client.js)
+5. Hydration (React "takes over" the existing rendered app, binds event handlers, etc.)
--- webpack.base.js
+++ webpack.base.js
@@ -3,14 +3,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js?$/,
+        test: /\.js?$/, // only .js files
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
+           // rules for transpaling 
           presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions']}}]
+            'react', // jsx to js
+            'stage-0', // handles async
+            [
+              'env', { 
+                targets: { // runs all transform rules needed for the last 2 versions of the browsers
+                  browsers: ['last 2 versions']
+                }
+              }
+            ] 
           ]
         }
       }
